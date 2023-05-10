e4b02d669b41e9782d422f51d34ff4d57932a27f
Imran <umurzakov117ali@gmail.com>
2023-05-10 21:56:48 +0500

    add new adress github

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: 'https://github.com/Imran117/github-finder.git',
   transpileDependencies: true
 })
